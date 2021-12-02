72c5e6062b236234682b861d57241a856a02cda6
Christopher <chris.tryber@gmail.com>
2021-12-02 10:46:19 -0300

    Requisito 10

--- src/api/app.js
+++ src/api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const user = require('../controller/routes/userRoutes');
 const login = require('../controller/routes/loginRoutes');
@@ -9,6 +10,8 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const uploadsPath = path.join(__dirname, '..', 'uploads');
+app.use('/images', express.static(uploadsPath));
 app.use('/users', user.router);
 app.use('/login', login.router);
 app.use('/recipes', recipe.router);
